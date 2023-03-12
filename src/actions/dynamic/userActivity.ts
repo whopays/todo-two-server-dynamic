13ef7775d7da9273d0f2cd1b321c9d574769b26e
demns <demn_@tut.by>
2023-03-12 19:37:39 +0100

    adds basic api

--- src/actions/dynamic/userActivity.ts
+++ src/actions/dynamic/userActivity.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express';
+import Todo from '../../schemas/Todo';
 import TodoList from '../../schemas/TodoList';
 
 const minute = 1 * 60 * 1000;
 
 interface Connection {
-  ip: string;
+  ip: string; // not unique, do base64
   startTimestamp: number;
 }
 
 interface ToDoListConnections {
   todoListId: TodoList['id'];
+  todoId: Todo['id'] | undefined;
   connections: Array<Connection>;
 }
 
@@ -20,6 +22,7 @@ startDroppingConnections();
 export default async (req: Request, res: Response) => {
   const { params } = req;
   const { todoListId } = params;
+  console.log(req.ip);
 
   const headers = {
     'Content-Type': 'text/event-stream',
@@ -30,18 +33,24 @@ export default async (req: Request, res: Response) => {
   res.write(`data: ${JSON.stringify({})}\n\n`);
 };
 
+// this streams events, this is where user subscribes
+// don't expose IP
+function todoListStream() {}
+
+// this is not exposed to end used
 function startDroppingConnections() {
   setInterval(() => {
     currentlyOpenedTodoLists.forEach(({ connections, todoListId }) => {
       connections.forEach(({ ip, startTimestamp }) => {
         if (Date.now() - startTimestamp < minute * 10) {
-          removeConnection(todoListId, ip);
+          removeConnection({ todoListId, ip });
         }
       });
     });
   }, minute);
 }
 
+// this is not exposed to end user
 function removeConnection({
   todoListId,
   ip,
@@ -52,12 +61,22 @@ function removeConnection({
   console.log('removed', todoListId, ip);
 }
 
-// function addConnection({
-//   todoListId,
-//   ip,
-// }: {
-//   todoListId: ToDoListConnections['todoListId'];
-//   ip: Connection['ip'];
-// }) {
-//   console.log('removed', todoListId, ip);
-// }
+function addConnection({
+  todoListId,
+  ip,
+}: {
+  todoListId: ToDoListConnections['todoListId'];
+  ip: Connection['ip'];
+}) {
+  console.log('added', todoListId, ip);
+}
+
+function changeActiveTodoInConnection({
+  todoListId,
+  ip,
+}: {
+  todoListId: ToDoListConnections['todoListId'];
+  ip: Connection['ip'];
+}) {
+  console.log('change todo');
+}
